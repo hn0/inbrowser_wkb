a20c63a61a3372f29e0304dda70a64a5440f0308
Hrvoje Novosel <hrvojedotnovosel@gmail.com>
2017-12-04 10:38:21 +0100

    + wkb test method

--- test.py
+++ test.py
@@ -6,9 +6,12 @@
     Created:  3. Dec 2017
 """
 
+import io
+import time
 import json
+import struct
 import urllib.request
-
+from osgeo import ogr
 
 SERVER = 'http://localhost:8000'
 
@@ -31,18 +34,45 @@ def test_wkb():
         Tests wkb geometry response
     """
     print( 'Testing wkb geometry type' )
-
+    start     = time.time()
+    geocnt    = 0
+    recordcnt = 0
     with urllib.request.urlopen( SERVER +  "/wkb" ) as fp:
         # TODO: create read buffer!!!
-        print( fp.read() )
+        ba = io.BytesIO( fp.read() )
 
-    print( 'WKB test done successfully' )
+        while 1:
+            id = struct.unpack( 'i', ba.read( 4 ) )[0]
+            n  = struct.unpack( 'i', ba.read( 4 ) )[0]
+            if id * n == 0:
+                break
+            recordcnt += 1
+            geo = ogr.CreateGeometryFromWkb( ba.read( n ) )
+            if geo.IsValid() and not geo.IsEmpty():
+                geocnt += 1
+            # print( (id, n) )
+
+    print( 'Got {} records from which {} geometries is valid'.format( recordcnt, geocnt ) )
+    print( 'WKB test done successfully, exec time: {}'.format( round( time.time() - start, 5) * 10000 ) )
+
+
+def test_wkt():
+    """
+        Tests wkt request
+    """
+    print( 'Testing wkt geometry type' )
+    start = time.time()
+    with urllib.request.urlopen( SERVER + "/wkt" ) as fp:
+        print( fp.read() )
+    raise Exception( 'Not implemented yet exception' )
+    print( 'WKT test done successfully, exec time: {}'.format( round( time.time() - start, 5) * 10000 ) )
 
 
 if __name__ == '__main__':
     try:
         test_server()
         test_wkb()
+        test_wkt()
     except Exception as ex:
         print( 'Test failed!' )
         print( ex )
