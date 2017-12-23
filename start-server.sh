3d2abe7db39f9e47ec80d469a00386788b1319ac
Hrvoje Novosel <hrvojedotnovosel@gmail.com>
2017-12-23 22:11:01 +0100

    * switch to docker enviroment

--- start-server.sh
+++ start-server.sh
@@ -5,17 +5,20 @@
 #  Created: 19. Nov 2017
 #
 
+which docker >> /dev/null 2>&1
+if [[ "$?" -eq 1 ]]; then
+    echo "Expecting docker binaries present on the system, exiting ..."
+    exit
+fi
 
-SERVER_PATH=$PWD"/server"
+ln=$(docker images | grep 'hn0stuff/go_gdal' | wc -l)
+if [[ "$ln" != 1 ]]; then
+    docker pull hn0stuff/go_gdal:1.0
+fi
 
-if [ "$GOPATH" != "$SERVER_PATH" ]; then 
-    export GOPATH="$SERVER_PATH"
-fi 
-
-cd $SERVER_PATH
-echo "Starting server ..."
-
-
-# usage app.go geometry_dataset static_files_folder
-go run src/app/app.go ../data/sample_data.sqlite ../client
-
+docker run --rm -d \
+           -p 8000:8000 \
+           -v $(dirname $(readlink -e "$0")):/usr/src/myapp \
+           -w "/usr/src/myapp/server" \
+           -e GOPATH="/usr/src/myapp/server" \
+           hn0stuff/go_gdal:1.0 go run src/app/app.go ../data/sample_data.sqlite ../client
