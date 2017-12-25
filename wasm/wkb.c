9743d6d116f038b351c67a77e12361ac9eeaf2f2
Hrvoje Novosel <hrvojedotnovosel@gmail.com>
2017-12-25 16:06:00 +0100

    * mocking aroud and braking wasm apart

--- wasm/wkb.c
+++ wasm/wkb.c
@@ -7,8 +7,27 @@
 */
 
 #include<stdio.h>
+#include "gdal.h"
 
-void convert( char* wkb, int len )
+char* type( long* wkb )
 {
-    printf( "Length of array is %d\n", sizeof( wkb ) );
+
+    OGRGeometryH hGeo;
+
+    hGeo = OGR_G_CreateGeometry( wkbMultiPolygon );
+
+    OGR_G_DestroyGeometry( hGeo );
+
+    // now we need a type!
+    // printf( "Length of array element is %d, byte order %04x and type %i \n", sizeof( wkb ), wkb[0]&0x1, wkb[0]&0x01 );
+
+    return "EMPTY";
+}
+
+char* convert( char* wkb, int len )
+{
+    // TODO: see if usage of gdal libs is feasible?!
+    printf( "Length of array element is %d and complete length of blob is %d\n", sizeof( wkb ), len );
+
+    return "ok";
 }
